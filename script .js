679446eac01a592c6b4c1abd7136a47c37e41ea4
AEJAZ7 <aejazjazz687@gmail.com>
2023-07-31 08:22:10 -0700

    first commit

---
+++ script .js
@@ -0,0 +1,41 @@
+
+
+// let slide = document.querySelectorAll(".customer");
+// let count = 0;
+// console.log(slide);
+
+// slide.forEach(function(customer, index){
+//     customer.style.left=`${index * 100}%`
+// })
+
+// function next(){
+//     count ++;
+//     if(count == slide.length){
+//         count=0;
+//     }
+//     bar()
+// }
+
+// function pre(){
+//     count--;
+//     if(count == -1){
+//         count = slide.length-1
+//     }
+//     bar()
+// }
+
+
+// function bar(){
+//     slide.forEach(function(customer){
+//         customer.style.transform = `translateX(-${count * 100}%)`
+//     })
+// }
+
+// let a = {
+//     greet:"HI"
+// }
+// let c;
+// c=a;
+// a.age=11;
+
+// console.log(c.age);
